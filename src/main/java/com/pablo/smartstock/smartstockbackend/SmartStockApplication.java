b38954971aa90517baf32a8c7740d21ec2ebdcb1
pabloordenes <pabloinfortech@gmail.com>
2025-08-02 18:07:45 -0400

    Proyecto base SmartStock backend (Spring Boot + Gradle)

---
+++ src/main/java/com/pablo/smartstock/smartstockbackend/SmartStockApplication.java
@@ -0,0 +1,13 @@
+package com.pablo.smartstock.smartstockbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartStockApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SmartStockApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/pablo/smartstock/smartstockbackend/SmartStockApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pablo.smartstock.smartstockbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartStockApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
